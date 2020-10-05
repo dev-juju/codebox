088fa66d2313f56a3733a8944dc63f67f4244f7d
Michele Riva <master@micheleriva.it>
2020-10-05 20:28:38 +0200

    fix: fixed code highlighting

--- src/index.js
+++ src/index.js
@@ -131,6 +131,25 @@ class CodeBox {
 
   _highlightCodeArea(event){
     hljs.highlightBlock(this.codeArea);
+    this._placeCaretAtEnd(this.codeArea);
+  }
+
+  _placeCaretAtEnd(el) {
+    el.focus();
+    if (typeof window.getSelection != "undefined" &&
+      typeof document.createRange != "undefined") {
+      const range = document.createRange();
+      range.selectNodeContents(el);
+      range.collapse(false);
+      const sel = window.getSelection();
+      sel.removeAllRanges();
+      sel.addRange(range);
+    } else if (typeof document.body.createTextRange != "undefined") {
+      const textRange = document.body.createTextRange();
+      textRange.moveToElementText(el);
+      textRange.collapse(false);
+      textRange.select();
+    }
   }
 
   _handleCodeAreaPaste(event){
